3b37cbc786e5d2777610ea3d074073bbb9a5ae06
fermontenegro <f.montengrozubieta@uandresbello.edu>
2022-07-04 16:45:14 -0400

    modificacion de readme y componentes

--- src/App.js
+++ src/App.js
@@ -52,7 +52,6 @@ function App() {
 
         <div className='iframe'>
         <iframe width="560" height="315" src="https://www.youtube.com/embed/Ati7Bq8FGQI" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-
         </div>
 
       </div>
--- src/componentes/carrusel.js
+++ src/componentes/carrusel.js
@@ -4,27 +4,28 @@ import c2 from '../imagenes/c2.jpg';
 import c3 from '../imagenes/c3.jpg';
 import c4 from '../imagenes/c4.jpg';
 import c5 from '../imagenes/c5.jpg';
-
+import swal from "sweetalert";
 
 const Carrusel = () => {
+
     return (
         <div className='carrusel'>
           <div id="carouselExampleFade" class="carousel slide carousel-fade" data-bs-ride="carousel">
             <div class="carousel-inner">
               <div class="carousel-item active">
-                <img className='imgcarrusel' src={c1} />
+                <img className='imgcarrusel' src={c1} useMap="#ss" />
               </div>
               <div class="carousel-item">
-                <img className='imgcarrusel' src={c2} />
+                <img className='imgcarrusel' src={c2} useMap="#mapsol"/>
               </div>
               <div class="carousel-item">
-                <img className='imgcarrusel' src={c3} />
+                <img className='imgcarrusel' src={c3} useMap="#tierra"/>
               </div>
               <div class="carousel-item">
-                <img className='imgcarrusel' src={c4} />
+                <img className='imgcarrusel' src={c4} useMap="#map"/>
               </div>
               <div class="carousel-item">
-                <img className='imgcarrusel' src={c5} />
+                <img className='imgcarrusel' src={c5} useMap="#map"/>
               </div>
             </div>
             <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
@@ -36,6 +37,14 @@ const Carrusel = () => {
               <span class="visually-hidden">Next</span>
             </button>
           </div>
+
+        <map name="ss">
+          <area shape="circle" href="" coords="300,200,100" alt="" onClick={swal("este es el sitema solar")}/>
+        </map>
+
+        <map name="mapsol">
+          <area shape="circle" href="" coords="300,200,100" alt="" onClick={swal("Una de las imágenes, tomada por el Extreme Ultraviolet Imager (EUI), es la imagen de mayor resolución jamás tomada del disco completo del Sol y de su atmósfera exterior, la corona")}/>
+        </map>
 
         </div>
 
